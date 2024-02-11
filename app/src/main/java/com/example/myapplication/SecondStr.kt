8e8732e2f787746cb468b69fdc136f8a2bc8a618
ljubi <ljubivaya@outlook.com>
2024-02-11 19:16:10 +0300

    1 commits

---
+++ app/src/main/java/com/example/myapplication/SecondStr.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class SecondStr {
+
+}
+@Preview
+@Composable
+fun Second() {
+    Text (text = "Hello")
+}
